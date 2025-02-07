ca44dd51eaaae226959e70f6fac0779d2701585a
gitTaynara4x <git.taynara.4x4@gmail.com>
2025-02-07 12:33:31 -0300

    Update main.py

--- main.py
+++ main.py
@@ -2,10 +2,21 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
+# Token de segurança (substitua pelo token real)
+VALID_TOKEN = "8i5l6yr9laqc0oi2y06vu1eg6003koo7"
+
 # Rota que irá tratar os eventos de webhook
 @app.route("/handler", methods=["POST"])
 def handle_webhook():
     print(f"🔔 Requisição recebida: {request.method} {request.path}")
+    
+    # Verifique o token no cabeçalho da requisição
+    token = request.headers.get('Authorization')  # Se o token estiver no cabeçalho Authorization
+    
+    if token != f"Bearer {VALID_TOKEN}":
+        print("⚠️ Token inválido!")
+        return jsonify({"status": "Unauthorized"}), 401
+
     if request.method == "POST":
         # Captura os dados do webhook enviados pela Bitrix
         data = request.json
